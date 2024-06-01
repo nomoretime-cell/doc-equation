bac1fdfd992e0f0fe12040d095019fdfb23d776f
遥奕 <jibing.yjb@alibaba-inc.com>
2024-06-01 16:41:20 +0800

    [codereview] [taskId]: none [taskUrl]: none [describe]: add debug log

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import logging
+import os
+import threading
 from equation.models import ModelInfo, load_nougat_model
 from equation.nougat import inner_process
 from equation.settings import settings
@@ -7,6 +10,10 @@ import io
 from pyfunvice import (
     app_service,
     start_app,
+)
+
+logging.basicConfig(
+    level=logging.INFO, format="[%(asctime)s] [%(thread)d] [%(levelname)s] %(message)s"
 )
 
 
@@ -20,14 +27,30 @@ def post_fork_func():
 
 @app_service(path="/api/v1/parser/ppl/equation", inparam_type="flat")
 async def process(
+    requestId: str,
     content_base64: str,
     type_block_idx: int,
     type_block_num: int,
     block_idx: int,
 ):
+    logging.info(
+        "POST request"
+        + f" [P{os.getpid()}][T{threading.current_thread().ident}][{requestId}] "
+        + f"type_block_idx: {type_block_idx}"
+        + f", type_block_num: {type_block_num}"
+        + f", block_idx: {block_idx}"
+    )
     image_byte = base64.b64decode(content_base64)
     image = io.BytesIO(image_byte)
     text = inner_process([image], [2048], model, 1)
+    logging.info(
+        "Return result"
+        + f" [P{os.getpid()}][T{threading.current_thread().ident}][{requestId}] "
+        + f"type_block_idx: {type_block_idx}"
+        + f", type_block_num: {type_block_num}"
+        + f", block_idx: {block_idx}"
+        + f", text: {text}"
+    )
     return {"text": text}
 
 
